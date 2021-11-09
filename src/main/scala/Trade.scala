92df7446c60338a9466a07489e8c58908fa97448
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-11-09 09:29:34 +0000

    small chnages

--- src/main/scala/Trade.scala
+++ src/main/scala/Trade.scala
@@ -1,9 +1,34 @@
 import java.util.Date
 
 // the scala implementation is simple as compared to java
-class Trade (var price: Double, val symbol: String, val id: Int, val quantity: Int){
+//constructor
+class Trade (var price: Double, val symbol: String, val id: Int, val quantity: Int) {
 
 
-  override def toString = s"Trade($price, $symbol, $id, $quantity)"
+  override def toString = s"Trade($price, $symbol, $id, $quantity)" //string interpolation
+  //ef ?!()// operators are methods eg +
+  // def apply(x: Int): Int = x + 1
+  //case class can be generated or instance without the new keyword
+ // exception
+//  try {
+//    val x: String = null
+//    x.length
+//  }catch {
+//    case e:Exception => " some faculty error message"
+//  }finally {
+//    //execute some code no matter what !
+//  }
+
+//  //generics
+//  abstract class myList[T]{
+//    def head: T
+//    def tail: myList[T]
+//  }
+//  val aList: List[Int] = List(1,2,3,4) // list.apply(1,2,3,4)
+//  val first = aList.head //int
+//  val rest = aList.tail
+//  val aStringList = List("HELLO","HELLO I AM HERE")
+//  val firstString = aStringList.head //string
+
 
 }
--- src/main/scala/myObject.scala
+++ src/main/scala/myObject.scala
@@ -1,7 +1,9 @@
 //A singleton object that extends the App class
-object myObject extends App {
+object myObject extends App { //because we have a static main method in App
   val tradeInstance  = new Trade(34.4,"V",56,45)
   println(s"The trade  has = : $tradeInstance")
+
+//  val mytrade = new Trade()...........
 
 
 //  val s = new Person("edward")
@@ -12,6 +14,19 @@ object myObject extends App {
 //  val p1: List[String] = people.map(p => p.name)
 //  val p2: List[String] = p1.filter(n => n.length > 5)
 //    p1.foreach(println)
+
+  // val gives you only reader
+  // var gives you a reader and a writer
+  // a constructor with no argument is a default constructor
+
+  //Auxiliary constructors
+//  def this()={
+//    this(1,1,1)
+//  }
+//
+//  def this(w:Int, h: Int){
+//    this.(w,h,1)
+//  }
 
 
 
