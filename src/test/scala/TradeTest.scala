c8c2b291be00a3e5cecdb1a157e7d3b98c1ec344
Akorlie Edward Tsatsu <65318539+edwardtsatsu@users.noreply.github.com>
2021-11-10 13:33:30 +0000

    wrote test for the trade class to test the setter

--- src/test/scala/TradeTest.scala
+++ src/test/scala/TradeTest.scala
@@ -1,13 +1,13 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class TradeTest extends AnyFunSuite{
-  val myTrade1 = new Trade(78,"vic","valid1",67)
+  val myTrade1 = new Trade(79,"vic","valid1",67)
 
   //describe independent test
   test("The price of the trade class should be 78") {
-    assert(myTrade1.price.==(78))
+    assert(myTrade1.price.==(79))
     assert(myTrade1.quantity.equals(67))
-    assert(myTrade1.value.==(5226))
+    assert(myTrade1.value.==(5293))
 
   }
 
